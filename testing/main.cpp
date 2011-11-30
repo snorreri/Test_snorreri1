7dac4cf12b81e071f83e39b3d129f6f34e47598f
Snorre Rist <snorreri@stud.ntnu.no>
2011-11-30 13:20:26 +0100

    Initial Commit

---
+++ testing/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  testing
+//
+//  Created by Snorre Rist on 30.11.11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
